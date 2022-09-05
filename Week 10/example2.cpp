908babbf1004045a179bb58abd7acb5ee63b7d9d
4111978+Fred-Wu@users.noreply.github.com <4111978+Fred-Wu@users.noreply.github.com>
2022-09-05 23:21:50 +1000

    update Week10 examples

--- Week 10/example2.cpp
+++ Week 10/example2.cpp
@@ -8,7 +8,7 @@ int main() {
     int sum = 0, a[100] = {0};
     
     for (int i = 2; i < 100; i++) {
-        sum = i;
+        sum = i;    // each number's multipler, 2*2 = 2+2, 2*3 = 2+2+2, 4*5 = 4+4+4+4+4
         while (sum < 100) {
             sum = sum + i;
             if (sum < 100) a[sum] = 1;
