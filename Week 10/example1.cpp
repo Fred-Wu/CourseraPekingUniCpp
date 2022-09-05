1ec692bdfdfcd649f9357825dcb31ab59a57ebb7
4111978+Fred-Wu@users.noreply.github.com <4111978+Fred-Wu@users.noreply.github.com>
2022-09-05 20:13:02 +1000

    Update Week10

---
+++ Week 10/example1.cpp
@@ -0,0 +1,18 @@
+// 在键盘上输入20个0-9之间的整数， 统计每个数在输入数列中出现的次数
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int num[n];
+    
+    for (int i = 0; i < n; i++) {
+        
+    }
+    
+    return 0;
+}
