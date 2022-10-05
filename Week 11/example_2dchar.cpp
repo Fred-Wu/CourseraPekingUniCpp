ce691f43de6a64302296d934c283389d70857b3d
4111978+Fred-Wu@users.noreply.github.com <4111978+Fred-Wu@users.noreply.github.com>
2022-10-05 12:47:43 +1100

    2dchars cout

---
+++ Week 11/example_2dchar.cpp
@@ -0,0 +1,16 @@
+//2-D 数组
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char weekday[7][11] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                           "Friday", "Saturday"};
+    
+    for (int i = 0; i < 7; i++) {
+        cout << weekday[i] << endl;
+    }
+    
+    return 0;
+}
